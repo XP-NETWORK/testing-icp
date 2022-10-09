6015deb7a654edde57f9576ef9f393cf5de05694
imsk17 <imsumitkumar17@gmail.com>
2022-10-09 16:59:13 +0530

    scripts: fix approve

--- src/approve.ts
+++ src/approve.ts
@@ -1,6 +1,6 @@
-import { Config, setup } from './index';
-import { NftInfo } from 'xp.network';
-import { DfinityNft } from 'xp.network/dist/helpers/dfinity/dfinity';
+import { Config, setup } from "./index";
+import { NftInfo } from "xp.network";
+import { DfinityNft } from "xp.network/dist/helpers/dfinity/dfinity";
 
 /**
  * A preTransfer function implementation
@@ -9,54 +9,54 @@ import { DfinityNft } from 'xp.network/dist/helpers/dfinity/dfinity';
  * @param mintWith destination contract address to mint with
  * @returns string | undefined
  */
-const Approve = async (to: string, nft: NftInfo<DfinityNft>, mintWith: string): Promise<string | undefined> => {
+const Approve = async (
+  to: string,
+  nft: NftInfo<DfinityNft>,
+  mintWith: string,
+): Promise<string | undefined> => {
+  const {
+    dfinity,
+    wallet,
+  } = await setup();
 
-    const {
-        dfinity,
-        wallet
-    } = await setup();
+  const estimation = await dfinity.estimateValidateTransferNft(
+    to,
+    nft,
+    mintWith,
+  );
 
-    const estimation = await dfinity.estimateValidateTransferNft(
-        to,
-        nft,
-        mintWith
-    );
+  const result = await dfinity.preTransfer(
+    wallet,
+    nft,
+    estimation,
+  );
 
-    const result = await dfinity.preTransfer(
-        //@ts-ignore
-        wallet,
-        nft,
-        estimation
-    );
-
-    return result;
-
-}
+  return result;
+};
 
 // Calling the Approve function
 (async () => {
+  // Destination Address
+  const to: string = Config.dfinity.to!;
 
-    // Destination Address
-    const to: string = Config.dfinity.to!;
+  // Selected NFT
+  const nft = {
+    uri: Config.dfinity.url,
+    native: {
+      canisterId: Config.dfinity.umt,
+      tokenId: Config.dfinity.tokenId,
+    },
+    collectionIdent: Config.dfinity.umt,
+  } as NftInfo<DfinityNft>;
 
-    // Selected NFT
-    const nft = {
-        uri: Config.dfinity.url,
-        native: {
-            canisterId: Config.dfinity.umt,
-            tokenId: Config.dfinity.tokenId
-        },
-        collectionIdent: Config.dfinity.umt
-    } as NftInfo<DfinityNft>;
+  // Destination contract address to mint with
+  const mintWith: string = Config.dfinity.mintWith!;
 
-    // Destination contract address to mint with
-    const mintWith: string = Config.dfinity.mintWith!;
+  const Result = await Approve(to, nft, mintWith);
+  console.log(Result);
 
-    const Result = await Approve(to, nft, mintWith);
-    console.log(Result);
-
-    process.exit(0);
-})().catch(e => {
-    console.error(e);
-    process.exit(1);
-})
+  process.exit(0);
+})().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
